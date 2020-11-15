9b36f484407d33fa85eb42cbf3d47a3015f7539e
Wjinlei <1976883731@qq.com>
2020-11-16 01:37:49 +0800

    Add markdown, org layer

--- init.el
+++ init.el
@@ -41,8 +41,8 @@ values."
      better-defaults
      emacs-lisp
      git
-     ;; markdown
-     ;; org
+     markdown
+     org
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
